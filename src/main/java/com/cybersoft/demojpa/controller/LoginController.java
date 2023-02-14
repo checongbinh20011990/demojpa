8935960f6f32b914a2b44351a4ee76fea7a352e1
checongbinh20001990 <checongbinh20011990@gmail.com>
2023-02-14 20:55:26 +0700

    fixed

--- src/main/java/com/cybersoft/demojpa/controller/LoginController.java
+++ src/main/java/com/cybersoft/demojpa/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.cybersoft.demojpa.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestParam("/api/ABC")
 public class LoginController {
     public void Login(){
 
